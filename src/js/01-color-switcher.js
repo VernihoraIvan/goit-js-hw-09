1439c939f707bbdc569aae5fd00f9ef447aad4df
VernihoraIvan <vernihorai@gmail.com>
2023-04-15 14:37:58 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const stopBtn = document.querySelector('[data-stop]');
 
+stopBtn.setAttribute('disabled', '');
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+const body = document.querySelector('body');
+let interval = null;
+function onStartBtn() {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
+  interval = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function onStopBtn() {
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled', '');
+  clearInterval(interval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
